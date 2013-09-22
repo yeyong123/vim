9d0152453e869f87683ac44c054664c310c52944
yeyong14 <yeyong14@gmail.com>
2013-09-22 16:47:08 +0800

    调试IE显示效果

---
+++ sample/first-one/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstOne::Application.config.secret_token = 'd2e066373e84a0adf7e6d4932300c42e140d987b07c172bfda7ccfe59fd765def9628a55f28dfb84a133b6d230732d43b887dc5f7590ea4f674d314a74840f63'
